caeac7647c72bba0d8b6c6fcaab9f42e869067c2
kohai <916309979@qq.com>
2022-02-18 17:19:10 +0800

    feat(ui/lazy): add component lazy

---
+++ packages/varlet-vue2-ui/src/lazy/example/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="example">
+    <app-type>{{ pack.basicUsage }}</app-type>
+    <img class="cat" v-lazy="'https://varlet.gitee.io/varlet-ui/cat.jpg'" />
+    <img class="cat" v-lazy="'https://varlet.gitee.io/varlet-ui/cat.jpg'" />
+    <img class="cat" v-lazy="'https://varlet.gitee.io/varlet-ui/cat.jpg'" />
+    <img class="cat" v-lazy="'https://varlet.gitee.io/varlet-ui/cat.jpg'" />
+    <img class="cat" v-lazy="'https://varlet.gitee.io/varlet-ui/cat.jpg'" />
+
+    <app-type>{{ pack.backgroundImageLazyLoad }}</app-type>
+    <div class="cat" v-lazy:background-image="'https://varlet.gitee.io/varlet-ui/cat.jpg'"></div>
+  </div>
+</template>
+
+<script>
+import Lazy from '..'
+import AppType from '@varlet-vue2/cli/site/mobile/components/AppType'
+import { use, pack } from './locale'
+import { watchLang } from '@varlet-vue2/cli/site/utils'
+
+export default {
+  name: 'LazyExample',
+  directives: { Lazy },
+  components: { AppType },
+  data: () => ({
+    pack,
+  }),
+  created() {
+    watchLang(this, use)
+  },
+  // setup() {
+  //   watchLang(use)
+
+  //   return { pack }
+  // },
+}
+</script>
+
+<style scoped lang="less">
+.cat {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  background-size: cover;
+  pointer-events: none;
+}
+</style>
