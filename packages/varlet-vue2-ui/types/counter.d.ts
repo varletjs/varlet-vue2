fb56ae594ecf40d6f4bd9a89e0553f3fd2191335
ayang <473033518@qq.com>
2022-02-22 13:57:17 +0800

    docs: new counter component types of files

---
+++ packages/varlet-vue2-ui/types/counter.d.ts
@@ -0,0 +1,44 @@
+import { VarComponent } from './varComponent'
+
+export type CounterValidateTriggers = 'onIncrement' | 'onDecrement' | 'onInputChange' | 'onLazyChange'
+
+export interface CounterProps {
+  value?: string | number
+  min?: string | number
+  max?: string | number
+  step?: string | number
+  color?: string
+  inputWidth?: string | number
+  inputTextSize?: string | number
+  buttonSize?: string | number
+  decimalLength?: string | number
+  disabled?: boolean
+  readonly?: boolean
+  disableIncrement?: boolean
+  disableDecrement?: boolean
+  disableInput?: boolean
+  lazyChange?: boolean
+  incrementButton?: boolean
+  decrementButton?: boolean
+  press?: boolean
+  ripple?: boolean
+  validateTrigger?: Array<CounterValidateTriggers>
+  rules?: Array<(v: number) => any>
+  onBeforeChange?: (value: number, change: (value: string | number) => void) => void
+  onChange?: (value: number) => void
+  onIncrement?: (value: number) => void
+  onDecrement?: (value: number) => void
+  onInput?: (value: number) => void
+}
+
+export class Counter extends VarComponent {
+  $props: CounterProps
+
+  validate(): Promise<boolean>
+
+  resetValidation(): void
+
+  reset(): void
+}
+
+export class _CounterComponent extends Counter {}
