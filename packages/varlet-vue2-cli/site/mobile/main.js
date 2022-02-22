84e80ed5ceca15ab849d75f7a3660999e72314d3
haoziqaq <357229046@qq.com>
2022-02-22 22:48:37 +0800

    fix(cli/site): mobile site add touch-emulator

--- packages/varlet-vue2-cli/site/mobile/main.js
+++ packages/varlet-vue2-cli/site/mobile/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@varlet/touch-emulator'
 
 import Icon from '../components/icon'
 import AppBar from '../components/app-bar'
