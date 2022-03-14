5db8d91073bfefcb02f2b0917cbf4ca4db601649
bilibili-ayang <75017711+bilibili-ayang@users.noreply.github.com>
2022-03-14 13:58:01 +0000

    Deploying to gh-page from @ varletjs/varlet-vue2@01e9910ed3f365a4fa8d9312cb2595614dd7f220 🚀

--- assets/index.b82c43fb.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./vendor.10bd23ac.js";const o={locks:{},zIndex:2e3,touchmoveForbid:!0},r=e.observable(o);export{r};
---
+++ assets/index.bab5582f.js
@@ -0,0 +1,1 @@
+import{a as e}from"./vendor.7d1cc22d.js";const o={locks:{},zIndex:2e3,touchmoveForbid:!0},r=e.observable(o);export{r};
