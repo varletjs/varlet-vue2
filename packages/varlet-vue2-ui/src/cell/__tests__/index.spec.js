47aa65d7df7e531b61a86d56b20e1305aadc1ec8
ayang <473033518@qq.com>
2022-03-15 10:39:18 +0800

    test(ui/cell): update snap

--- packages/varlet-vue2-ui/src/cell/__tests__/index.spec.js
+++ packages/varlet-vue2-ui/src/cell/__tests__/index.spec.js
@@ -15,28 +15,28 @@ test('test cell plugin', () => {
   expect(Vue.component(Cell.name)).toBeTruthy()
 })
 
-// test('test cell props', async () => {
-//   const wrapper = mount(VarCell, {
-//     propsData: {
-//       title: 'This is Cell',
-//       desc: 'This is desc',
-//       border: true,
-//       iconClass: 'test-icon-class',
-//       titleClass: 'test-title-class',
-//       descClass: 'test-desc-class',
-//       extraClass: 'test-extra-class',
-//       icon: 'fire',
-//     },
-//     scopedSlots: {
-//       extra: '<div>text</div>',
-//     },
-//   })
-//   expect(wrapper.find('.var-icon').exists()).toBe(true)
-//   expect(wrapper.find('.var-icon').classes('var-icon-fire')).toBe(true)
-//   expect(wrapper.classes('var-cell--border')).toBe(true)
-//   expect(wrapper.find('.var-cell__title').text()).toBe('This is Cell')
-//   expect(wrapper.find('.var-cell__desc').text()).toBe('This is desc')
-//   expect(wrapper.find('.test-extra-class').text()).toBe('text')
-//   expect(wrapper.html()).toMatchSnapshot()
-//   wrapper.destroy()
-// })
+test('test cell props', async () => {
+  const wrapper = mount(VarCell, {
+    propsData: {
+      title: 'This is Cell',
+      desc: 'This is desc',
+      border: true,
+      iconClass: 'test-icon-class',
+      titleClass: 'test-title-class',
+      descClass: 'test-desc-class',
+      extraClass: 'test-extra-class',
+      icon: 'fire',
+    },
+    scopedSlots: {
+      extra: '<div>text</div>',
+    },
+  })
+  expect(wrapper.find('.var-icon').exists()).toBe(true)
+  expect(wrapper.find('.var-icon').classes('var-icon-fire')).toBe(true)
+  expect(wrapper.classes('var-cell--border')).toBe(true)
+  expect(wrapper.find('.var-cell__title').text()).toBe('This is Cell')
+  expect(wrapper.find('.var-cell__desc').text()).toBe('This is desc')
+  expect(wrapper.find('.test-extra-class').text()).toBe('text')
+  expect(wrapper.html()).toMatchSnapshot()
+  wrapper.destroy()
+})
