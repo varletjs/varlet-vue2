6f53d7f37d3bc652b5b64fc1bfaf11de6cb80065
ayang <473033518@qq.com>
2022-03-15 17:25:41 +0800

    feat(ui/chip): update snap

--- packages/varlet-vue2-ui/src/chip/__tests__/index.spec.js
+++ packages/varlet-vue2-ui/src/chip/__tests__/index.spec.js
@@ -15,49 +15,49 @@ test('test chip plugin', () => {
   expect(Vue.component(Chip.name)).toBeTruthy()
 })
 
-// test('test chip color & textColor', () => {
-//   const wrapper = mount(VarChip, {
-//     propsData: {
-//       color: '#000',
-//       textColor: '#000',
-//     },
-//   })
+test('test chip color & textColor', () => {
+  const wrapper = mount(VarChip, {
+    propsData: {
+      color: '#000',
+      textColor: '#000',
+    },
+  })
 
-//   expect(wrapper.html()).toMatchSnapshot()
-// })
+  expect(wrapper.html()).toMatchSnapshot()
+})
 
-// test('test chip close', () => {
-//   const onClose = jest.fn()
+test('test chip close', () => {
+  const onClose = jest.fn()
 
-//   const wrapper = mount(VarChip, {
-//     propsData: {
-//       closable: true,
-//     },
-//     listeners: {
-//       close: onClose,
-//     },
-//   })
+  const wrapper = mount(VarChip, {
+    propsData: {
+      closable: true,
+    },
+    listeners: {
+      close: onClose,
+    },
+  })
 
-//   const closeEl = wrapper.find('.var-chip--close')
-//   expect(closeEl.exists()).toBe(true)
-//   closeEl.trigger('click')
-//   expect(onClose).toHaveBeenCalledTimes(1)
-// })
+  const closeEl = wrapper.find('.var-chip--close')
+  expect(closeEl.exists()).toBe(true)
+  closeEl.trigger('click')
+  expect(onClose).toHaveBeenCalledTimes(1)
+})
 
-// test('test chip styles', () => {
-//   const wrapper = mount(VarChip, {
-//     propsData: {
-//       closable: true,
-//       size: 'large',
-//       round: true,
-//       plain: true,
-//       block: true,
-//       iconName: 'fire',
-//     },
-//     scopedSlots: {
-//       default: '<div>chip text</div>',
-//     },
-//   })
+test('test chip styles', () => {
+  const wrapper = mount(VarChip, {
+    propsData: {
+      closable: true,
+      size: 'large',
+      round: true,
+      plain: true,
+      block: true,
+      iconName: 'fire',
+    },
+    scopedSlots: {
+      default: '<div>chip text</div>',
+    },
+  })
 
-//   expect(wrapper.html()).toMatchSnapshot()
-// })
+  expect(wrapper.html()).toMatchSnapshot()
+})
