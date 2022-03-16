209d4bdea65f652d597ac05966c3474ae4721510
zhenyuWang <13641039885@163.com>
2022-03-16 09:17:41 +0800

    test(ui/lazy): add test case and snapshots

--- packages/varlet-vue2-ui/src/lazy/__tests__/index.spec.js
+++ packages/varlet-vue2-ui/src/lazy/__tests__/index.spec.js
@@ -15,97 +15,97 @@ test('test lazy plugin', () => {
   expect(Vue.directive('lazy')).toBeTruthy()
 })
 
-// const Wrapper = {
-//   directives: { Lazy },
-//   data: () => ({
-//     src: 'https://varlet.gitee.io/varlet-ui/cat.jpg',
-//     error: 'https://varlet.gitee.io/varlet-ui/error.jpg',
-//   }),
-//   template: `
-//     <img
-//       :lazy-error="error"
-//       v-lazy="src"
-//     >
-//   `,
-// }
+const Wrapper = {
+  directives: { Lazy },
+  data: () => ({
+    src: 'https://varlet.gitee.io/varlet-ui/cat.jpg',
+    error: 'https://varlet.gitee.io/varlet-ui/error.jpg',
+  }),
+  template: `
+    <img
+      :lazy-error="error"
+      v-lazy="src"
+    >
+  `,
+}
 
-// test('test lazy load', async () => {
-//   const { mockRestore } = mockDoubleRaf()
-//   const wrapper = mount(Wrapper)
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+test('test lazy load', async () => {
+  const { mockRestore } = mockDoubleRaf()
+  const wrapper = mount(Wrapper)
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   await trigger(wrapper.element._lazy.preloadImage, 'load')
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+  await trigger(wrapper.element._lazy.preloadImage, 'load')
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   wrapper.destroy()
-//   imageCache.clear()
-//   mockRestore()
-// })
+  wrapper.destroy()
+  imageCache.clear()
+  mockRestore()
+})
 
-// test('test lazy error with attempt', async () => {
-//   const { mockRestore } = mockDoubleRaf()
-//   const wrapper = mount(Wrapper)
-//   expect(wrapper.html()).toMatchSnapshot()
+test('test lazy error with attempt', async () => {
+  const { mockRestore } = mockDoubleRaf()
+  const wrapper = mount(Wrapper)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   await delay(80)
+  await delay(80)
 
-//   await trigger(wrapper.element._lazy.preloadImage, 'error')
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+  await trigger(wrapper.element._lazy.preloadImage, 'error')
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   await trigger(wrapper.element._lazy.preloadImage, 'error')
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+  await trigger(wrapper.element._lazy.preloadImage, 'error')
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   await trigger(wrapper.element._lazy.preloadImage, 'error')
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+  await trigger(wrapper.element._lazy.preloadImage, 'error')
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   wrapper.destroy()
-//   imageCache.clear()
-//   mockRestore()
-// })
+  wrapper.destroy()
+  imageCache.clear()
+  mockRestore()
+})
 
-// test('test lazy updated', async () => {
-//   const { mockRestore } = mockDoubleRaf()
-//   const wrapper = mount(Wrapper)
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+test('test lazy updated', async () => {
+  const { mockRestore } = mockDoubleRaf()
+  const wrapper = mount(Wrapper)
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   await trigger(wrapper.element._lazy.preloadImage, 'load')
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+  await trigger(wrapper.element._lazy.preloadImage, 'load')
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   await wrapper.setData({ src: 'https://varlet.gitee.io/varlet-ui/dog.jpg' })
-//   await delay(80)
+  await wrapper.setData({ src: 'https://varlet.gitee.io/varlet-ui/dog.jpg' })
+  await delay(80)
 
-//   await trigger(wrapper.element._lazy.preloadImage, 'load')
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+  await trigger(wrapper.element._lazy.preloadImage, 'load')
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   wrapper.destroy()
-//   imageCache.clear()
-//   mockRestore()
-// })
+  wrapper.destroy()
+  imageCache.clear()
+  mockRestore()
+})
 
-// test('test lazy background-image', async () => {
-//   const { mockRestore } = mockDoubleRaf()
-//   const wrapper = mount({
-//     directives: { Lazy },
-//     template: `
-//       <img v-lazy:background-image="'https://varlet.gitee.io/varlet-ui/cat.jpg'">
-//     `,
-//   })
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+test('test lazy background-image', async () => {
+  const { mockRestore } = mockDoubleRaf()
+  const wrapper = mount({
+    directives: { Lazy },
+    template: `
+      <img v-lazy:background-image="'https://varlet.gitee.io/varlet-ui/cat.jpg'">
+    `,
+  })
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   await trigger(wrapper.element._lazy.preloadImage, 'load')
-//   await delay(80)
-//   expect(wrapper.html()).toMatchSnapshot()
+  await trigger(wrapper.element._lazy.preloadImage, 'load')
+  await delay(80)
+  expect(wrapper.html()).toMatchSnapshot()
 
-//   wrapper.destroy()
-//   imageCache.clear()
-//   mockRestore()
-// })
+  wrapper.destroy()
+  imageCache.clear()
+  mockRestore()
+})
